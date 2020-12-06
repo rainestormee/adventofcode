62c94efc36ebf3e3b4fe8e611a7a8a990c9d118d
rainestormee <ryanarrowsmith01@gmail.com>
2020-12-06 07:00:28 +0000

    renaming variables

--- day6.py
+++ day6.py
@@ -1,2 +1,2 @@
 from functools import reduce
-print("" if (groups := [[a.split("\n")] for a in open("./input/day6.txt").read()[:-1].split("\n\n")]) and print(["Part 1:", sum(sum(len(reduce(lambda x, y: set(list(x) + list(y)), list(a))) for a in b) for b in groups), "Part 2:", sum(sum(len(reduce(lambda x, y: list([v for v in x if v in y]), list(a))) for a in b) for b in groups)]) else "- Ryan Arrowsmith")
+print("" if (c := [[a.split("\n")] for a in open("./input/day6.txt").read()[:-1].split("\n\n")]) and print(["Part 1:", sum(sum(len(reduce(lambda x, y: set(list(x) + list(y)), list(a))) for a in b) for b in c), "Part 2:", sum(sum(len(reduce(lambda x, y: list([v for v in x if v in y]), list(a))) for a in b) for b in c)]) else "- Ryan Arrowsmith")
