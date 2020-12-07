81a517ca79f2a5b2612e0ef5f26d6a49de228d47
rainestormee <ryanarrowsmith01@gmail.com>
2020-12-07 08:29:11 +0000

    solve for 7.2

--- day7.py
+++ day7.py
@@ -1,40 +1,59 @@
-rulesRaw = [x for x in open("./input/day7.txt").read().split(".\n")]
+from functools import reduce
+
+rulesRaw = [x for x in open("./input/day7-test.txt").read().split(".\n")]
 
 print(rulesRaw)
 
 rules = {
 
 }
+amounts = {
+
+}
+bags = {
+
+}
 
 def walk(start_point):
     if start_point not in rules:
-        print(start_point, "not in")
         return False
     if "shiny gold bag" in rules[start_point] or "shiny gold bags" in rules[start_point]:
-        print(start_point, "found shiny")
         return True
     if rules[start_point][0] == "":
-        print(start_point, "dead end")
         return False
     return any(walk(x) for x in rules[start_point])
+
+bagList = []
+
+def count(outer_bag, multiplier):
+    return 0 if "" in bags[outer_bag] else sum([multiplier * (bags[outer_bag][bag] + count(bag, bags[outer_bag][bag])) for bag in bags[outer_bag]])
 
 for x in rulesRaw:
     rule = x.split("contain")
     container = rule[0].strip().replace("bags", "bag")
     inside = [x.strip()[2:].strip().replace(".", "").replace("other bags", "").replace("bags", "bag") for x in rule[1].split(", ")]
-    print(container, inside)
-    rules[container] = inside
+    amount = []
+    for y in rule[1].split(", "):
+        try:
+            amount.append(int(y[:2].strip()))
+        except ValueError:
+            amount.append(0)
 
+    rules[container] = inside
+    baggage = {}
+    for i in range(len(inside)):
+        baggage[inside[i]] = amount[i]
+
+    bags[container] = baggage
 counter = 0
 correct = []
 
 for x in rules:
-    print(x, rules[x])
-    print("Starting to walk for {}".format(x))
     if walk(x):
-        print("True")
         correct.append(x)
         counter += 1
 
+print(bags)
+
 if __name__ == "__main__":
-    print(counter)
+    print(count("shiny gold bag", 1))
